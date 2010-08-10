6c02cffa926af6936bf39d85034623390fbfff53
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-08-10 02:26:05 +0000

    * fix : build crawlzilla install shell

--- build-crawlzilla.sh
+++ build-crawlzilla.sh
@@ -21,7 +21,7 @@ function checkMethod(){
 
 SvnProject=/opt/svn_project
 GoogleCodeCrawlzilla=$SvnProject/crawlzilla
-TmpDir=Crawlzilla_Install
+TmpDir=/tmp/Crawlzilla_Install
 ShellTar=Crawlzilla-0.2-$DATE_VER-Shell.tar.gz
 FullTar=Crawlzilla-0.2-$DATE_VER-Full.tar.gz
 
