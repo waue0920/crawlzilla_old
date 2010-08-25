a1c58da3af797d8283cf3ed95c9d8757a3c7e52c
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-08-25 06:28:09 +0000

    wiki SystemInstall pictures

--- src/shell/rm_DB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $1 == " " ]; then
-echo "No parameter"
-else
-rm -f /home/crawler/crawlzilla/.tmp/$1
-rm -fr /opt/crawlzilla/tomcat/webapps/$1
-/opt/crawlzilla/nutch/bin/hadoop fs -rmr /user/crawler/$1
-fi
-
