2f5f85f4afd9042b1fa4a22d7ecead4d0b3e48b6
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-09-03 09:49:25 +0000

    * add license info to web code

--- src/web/src/org/nchc/crawlzilla/DataInfoBean.java
+++ src/web/src/org/nchc/crawlzilla/DataInfoBean.java
@@ -1,16 +1,31 @@
-package org.nchc.crawlzilla;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
- * DataInfoBean 內的成員有資料，由Statistics 設定，然後被 statistics.jsp 讀取
- * 
- * 會用到 IndexInfo 以及引用 org.apache.lucene.index.IndexReader 
- * 
+ * @guide
+ * The data of DataInfoBean is set by "Statistics" then read by "statistics.jsp"
+ * It uses "IndexInfo" and org.apache.lucene.index.IndexReader
  * @web
- * <a href="http://code.google.com/p/crawlzilla/">http://code.google.com/p/crawlzilla </a>
- * 
+ * http://code.google.com/p/crawlzilla 
  * @author Waue, Shunfa, Rock {waue, shunfa, rock}@nchc.org.tw
- * 
  */
+
+package org.nchc.crawlzilla;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
