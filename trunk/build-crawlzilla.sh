79c80675306963e1bb53d32d3eede4e37195761e
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-10-25 03:37:20 +0000

    * add build-crawlzilla.sh : lifting /opt/svn_project/crawlzilla restrictions

--- trunk/build-crawlzilla.sh
+++ trunk/build-crawlzilla.sh
@@ -20,12 +20,20 @@ function checkMethod(){
   fi
 }
 
+if [ -d /opt/svn_project/crawlzilla ]; then
+    SvnProject=/opt/svn_project
+    GoogleCodeCrawlzilla=$SvnProject/crawlzilla
+else
+    GoogleCodeCrawlzilla=`dirname "$0"`
+    GoogleCodeCrawlzilla=`cd "$GoogleCodeCrawlzilla"; pwd`
+    SvnProject=$GoogleCodeCrawlzilla/../
+fi
 
-SvnProject=/opt/svn_project
-GoogleCodeCrawlzilla=$SvnProject/crawlzilla
+exit
+
 TmpDir=Crawlzilla_Install
-ShellTar=Crawlzilla-0.2-$DATE_VER-Shell.tar.gz
-FullTar=Crawlzilla-0.2-$DATE_VER-Full.tar.gz
+ShellTar=Crawlzilla-$CURRENT_VER-$DATE_VER-Shell.tar.gz
+FullTar=Crawlzilla-$CURRENT_VER-$DATE_VER-Full.tar.gz
 
 # 1 同步資料與編譯web資料
 
@@ -77,47 +85,44 @@ cd $SvnProject
 tar -czvf $ShellTar $TmpDir --exclude=.svn
 checkMethod 5.1
 
-# 6 放入 pack 檔
+# 6 製作full package 檔
+echo "Make full-package, skip ..."
+checkMethod 6.1
 # mkdir $TmpDir/package
 # cp $GoogleCodeNutcheEz/src/package/crawlzilla-pack-current.tar.gz $TmpDir/package/
 # checkMethod 6.1
-
-# 7 壓縮完整包
-# 這邊先註解掉，因為這個包很大
+# 6.2 壓縮完整包
 # tar -czvf $FullTar $TmpDir --exclude=.svn
-# checkMethod 7.1
-
-
-# 8 複製到 trac 的 package
+# checkMethod 6.2
+# 6.3 複製到 trac 的 package
 #if [ ! -d $SvnProject/dist ];then
 #  mkdir $SvnProject/dist
-#  checkMethod 8.1
+#  checkMethod 6.3
 #fi
-
 # 先不做fulltar
 #if [ -f $SvnProject/dist/$FullTar ];then
 #  rm $SvnProject/dist/$FullTar;
-#  checkMethod 8.2
+#  checkMethod 6.4
 #fi
 #mv $FullTar $SvnProject/dist
-#checkMethod 8.3
+#checkMethod 6.5
 
+# 7 將製作好的安裝檔放在保存的資料夾
 if [ -f $SvnProject/dist/$ShellTar ];then
   rm $SvnProject/dist/$ShellTar;
-  checkMethod 8.4
+  checkMethod 7.0
 fi
 mv $ShellTar $SvnProject/dist
-checkMethod 8.5
-
-# 9 刪除 DELETE_LOCK=1
+checkMethod 7.1
+# 7.2 刪除 DELETE_LOCK=1
 if [ $DELETE_LOCK -eq 1 ];then
   rm -rf $TmpDir;
-  checkMethod 9.1
+  checkMethod 7.2
 fi
 
 
-# 10 上傳到 source forge
-# 做local user 與 sf.net 上的user 對應, fafa 與
+# 8 上傳到 source forge
+# 做local user 與 sf.net 上的user 對應, fafa 與 rock
 if [ $USER == "waue" ];then
     USER=waue0920;
 elif [ $USER == "rock" ];then
@@ -131,4 +136,4 @@ scp $SvnProject/dist/$ShellTar $USER,crawlzilla@frs.sourceforge.net:/home/frs/pr
 echo "完成，一切確認後，最後的檔案放在這個目錄內："
 echo "	../dist/ "
 echo "最新的版本[ $ShellTar ]也同時上傳到sf.net囉"
-echo "http://sourceforge.net/downloads/crawlzilla/testing/Crawlzilla-0.2/"
+echo "http://sourceforge.net/downloads/crawlzilla/testing/Crawlzilla-$CURRENT_VER/"
