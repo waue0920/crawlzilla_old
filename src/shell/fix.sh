10d226931cd9982ecb2b37d1b958cec541827ce7
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-11-04 07:01:49 +0000

    fix  fix process on  src/shell/fix.sh, src/shell/go.sh, build-crawlzilla.sh

--- src/shell/fix.sh
+++ src/shell/fix.sh
@@ -14,21 +14,30 @@ fi
 # begin
 
 JNAME=$1
-LOGFILE=~/crawlzilla/debug_fix.log
+LOGFILE=/var/log/crawlzilla/shell-logs/crawl_fix.log
+#LOGFILE=/home/crawler/crawlzilla/debug_fix.log # no used
 META_PATH=/home/crawler/crawlzilla/.tmp
 
 ### not test
-JPID="$META_PATH/$JNAME/$JNAME"_count_pid # go.sh need add go.sh's pid
-JDEPTH="$META_PATH/$JNAME/$JNAME"xxx # go.sh need fix
-JPTIME="$META_PATH/$JNAME/$JNAME"PassTime
+JPID=$(cat "$META_PATH/$JNAME/$JNAME"_go_pid) # go job pid 
+CPID=$(cat "$META_PATH/$JNAME/$JNAME"_count_pid) # count pid 
+JDEPTH=$(cat "$META_PATH/$JNAME/".crawl_depth) # depth
+STATUS_FILE=$META_PATH/$JNAME/$JNAME # status path
+#JPTIME="$META_PATH/$JNAME/$JNAME"PassTime
 ### not test
 
 
 DATE=$(date)
 echo "$JNAME BEGINE at $DATE" >> $LOGFILE
 
-echo "1 invertlinks" >> $LOGFILE
+echo "0 kill ps" >> $LOGFILE
+kill -9 $JPID
+if [ ! $? -eq 0 ];then echo "Warning!!! kill go.sh not work" >> $LOGFILE ; fi
+kill -9 $CPID
+if [ ! $? -eq 0 ];then echo "Warning!!! kill count.sh not work">> $LOGFILE; fi
+echo "fixing" > $STATUS_FILE;
 
+echo "1 invertlinks" >> $LOGFILE
 /opt/crawlzilla/nutch/bin/nutch invertlinks /user/crawler/$JNAME/linkdb -dir /user/crawler/$JNAME/segments/
 if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
 
@@ -45,32 +54,35 @@ echo "4 download" >> $LOGFILE
 /opt/crawlzilla/nutch/bin/hadoop dfs -get $JNAME /home/crawler/crawlzilla/archieve/$JNAME
 if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
 
-echo "5 $JNAMEPassTime" >> $LOGFILE
-echo "0h:0m:0s" >> /home/crawler/crawlzilla/archieve/$JNAME/$JNAME"PassTime"
-if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
 
-echo "6 append depth" >> $LOGFILE
-echo "0" >> /home/crawler/crawlzilla/archieve/$JNAME/.crawl_depth
-if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
+if [ ! -f /home/crawler/crawlzilla/archieve/$JNAME/$JNAME"PassTime" ];then
+    echo "4.1 $JNAMEPassTime" >> $LOGFILE
+    echo "0h:0m:0s" >> /home/crawler/crawlzilla/archieve/$JNAME/$JNAME"PassTime"
+fi
 
-echo "7 mv index files from part-00000" >> $LOGFILE
+if [ ! -f /home/crawler/crawlzilla/archieve/$JNAME/.crawl_depth ];then
+    echo "4.2 append depth" >> $LOGFILE
+    echo "0" >> /home/crawler/crawlzilla/archieve/$JNAME/.crawl_depth
+fi
+
+echo "5 mv index files from part-00000" >> $LOGFILE
 mv /home/crawler/crawlzilla/archieve/$JNAME/index/part-00000/* /home/crawler/crawlzilla/archieve/$JNAME/index/
 if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
 
-echo "8 rmdir part-00000/"  >> $LOGFILE
+echo "6 rmdir part-00000/"  >> $LOGFILE
 rmdir /home/crawler/crawlzilla/archieve/$JNAME/index/part-00000/
 if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
 
-echo "9 tomcat"  >> $LOGFILE
+echo "7 tomcat"  >> $LOGFILE
 cp -rf /opt/crawlzilla/tomcat/webapps/default /opt/crawlzilla/tomcat/webapps/$JNAME
 if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
 
-echo "10 nutch-site.xml"  >> $LOGFILE
+echo "8 nutch-site.xml"  >> $LOGFILE
 sed -i '8s/search/'${JNAME}'/g' /opt/crawlzilla/tomcat/webapps/$JNAME/WEB-INF/classes/nutch-site.xml
 if [ ! $? -eq 0 ];then echo "ERROR!!! see $LOGFILE ";exit 8; fi
 
 
-
+echo "fixed" > $STATUS_FILE;
 
 DATE=$(date)
 echo "$JNAME completed and finished at"$DATE >> $LOGFILE
