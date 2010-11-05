45f84d8480059006a5a446fe3271d0bd1ecb6363
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-11-05 08:10:22 +0000

    fix re_crawl.sh bug M    src/shell/fix.sh M    src/shell/re_crawl.sh

--- trunk/src/shell/re_crawl.sh
+++ trunk/src/shell/re_crawl.sh
@@ -14,16 +14,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-indexPoolName=$1
 
-if [ -d /home/crawler/crawlzilla/archieve/$indexPoolName ]; then
-  depth=$(cat /home/crawler/crawlzilla/archieve/$indexPoolName/.crawl_depth)
-  cp /home/crawler/crawlzilla/archieve/$indexPoolName/urls/urls.txt /home/crawler/crawlzilla/urls
-  rm -rf /home/crawler/crawlzilla/.tmp/$indexPoolName
-  rm -rf /opt/crawlzilla/tomcat/webapps/$indexPoolName
-  /opt/crawlzilla/nutch/bin/hadoop fs -rmr /user/crawler/$indexPoolName
-  ./go.sh $depth $indexPoolName
+
+# prompt
+if [ "$1" == "" ];then
+    echo "Usage : recrawl <JOB_NAME>";
+    echo " where JOB_NAME is one of: ";
+    echo "==========="
+	ls "/home/crawler/crawlzilla/archieve/"
+    echo "==========="
+    exit 9;
+fi
+
+# begin
+
+JNAME=$1
+LOGFILE=/var/log/crawlzilla/shell-logs/crawl_re.log
+DEPTH=$(cat /home/crawler/crawlzilla/archieve/$JNAME/.crawl_depth)
+DATE=$(date)
+
+echo "ReCrawl $JNAME BEGIN at $DATE" >> $LOGFILE
+
+echo "0 kill ps" >> $LOGFILE
+kill -9 $JPID
+
+
+if [ -d /home/crawler/crawlzilla/archieve/$JNAME ]; then
+  echo "1. copy urls" >> $LOGFILE
+    if [ -f /home/crawler/crawlzilla/urls/urls.txt ]; then
+	rm /home/crawler/crawlzilla/urls/urls.txt
+    fi
+  cp /home/crawler/crawlzilla/archieve/$JNAME/urls/urls.txt /home/crawler/crawlzilla/urls
+  if [ ! $? -eq 0 ];then echo "Error! copy urls.txt broken" >> $LOGFILE ; fi
+
+  echo "2. remove $JNAME metadata" >> $LOGFILE
+  rm -rf /home/crawler/crawlzilla/.tmp/$JNAME
+  if [ ! $? -eq 0 ];then echo "Error! " >> $LOGFILE ; fi
+
+  echo "3. remove /opt/crawlzilla/tomcat/webapps/$JNAME" >> $LOGFILE
+  rm -rf /opt/crawlzilla/tomcat/webapps/$JNAME
+  if [ ! $? -eq 0 ];then echo "Error!" >> $LOGFILE ; fi
+
+  echo "4. remove hdfs /user/crawler/$JNAME " >> $LOGFILE
+  /opt/crawlzilla/nutch/bin/hadoop fs -rmr /user/crawler/$JNAME
+  if [ ! $? -eq 0 ];then echo "Error! " >> $LOGFILE ; fi
+
+  echo "5. go.sh $DEPTH $JNAME " >> $LOGFILE
+  /home/crawler/crawlzilla/system/go.sh $DEPTH $JNAME
+  DATE=$(date)
+  echo "6. ReCrawl finish at $DATE" >> $LOGFILE
 else
-  echo "No Parameter of File not Find!"
+  echo "ERROR! $JNAME not found!" >> $LOGFILE
 fi
 
