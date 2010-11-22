9e0298b1fad1abadf0e700578ee1471708ba710d
goldjay1231 <goldjay1231@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-11-22 06:43:02 +0000

    Modify: absolute path -> relative path (build, ant)
    M    install_from_svn
    M    src/web/build.xml
    M    build-crawlzilla.sh

--- trunk/build-crawlzilla.sh
+++ trunk/build-crawlzilla.sh
@@ -3,24 +3,19 @@
 # 請先設定 DELETE_LOCK 是否要刪除
 # 以及安裝的時間版本 DATE_VER 
 
-SvnProject="" # 請輸入你的svn 專案目錄
-
 DELETE_LOCK=1 # 1= 刪除 $TmpDir 資料夾
 DATE_VER=`date +%y%m%d` # 年月日
 CURRENT_VER=0.3 # 專案目前的版本
 MINOR_VER=0
+# for ant
 
-if [  "a$SvnProject" == "a" ]; then
-    if [ -d /opt/svn_project/crawlzilla ]; then
-	SvnProject=/opt/svn_project
-	GoogleCodeCrawlzilla=$SvnProject/crawlzilla
-    else
-	GoogleCodeCrawlzilla=`dirname "$0"`
-	GoogleCodeCrawlzilla=`cd "$GoogleCodeCrawlzilla"; pwd`
-	SvnProject=`$GoogleCodeCrawlzilla/../`
-    fi
+if [ -d "opt/svn_project/crawlzilla" ]; then
+    GoogleCodeCrawlzilla=$/opt/svn_project/crawlzilla
+    read -p "1.1"
 else
-    GoogleCodeCrawlzilla=$SvnProject/crawlzilla
+    GoogleCodeCrawlzilla=`pwd "$0"`
+    echo GoogleCodeCrawlzilla=$GoogleCodeCrawlzilla
+    read -p "1.2"
 fi
 
 TmpDir=Crawlzilla_Install
@@ -47,19 +42,19 @@ checkMethod 1.2
 
 # 2 開始目錄以及生成暫存目錄
 
-cd $SvnProject
+#cd $SvnProject
 
 if [ -d $TmpDir ];then
   rm -rf $TmpDir;
   checkMethod 2.1
 fi
 mkdir $TmpDir
-
+mkdir $GoogleCodeCrawlzilla/dist 2> /dev/null
 # 2.5 打包 crawlzilla.war
 mkdir $TmpDir/web/
 checkMethod 2.2
-cp $SvnProject/dist/crawlzilla.war $TmpDir/web/
-mv $SvnProject/dist/crawlzilla.war $SvnProject/dist/crawlzilla-$DATE_VER.war
+cp $GoogleCodeCrawlzilla/src/web/dist/crawlzilla.war $TmpDir/web/
+mv $GoogleCodeCrawlzilla/src/web/dist/crawlzilla.war $GoogleCodeCrawlzilla/dist/crawlzilla-$DATE_VER.war
 checkMethod 2.3
 
 
@@ -79,11 +74,10 @@ checkMethod 4.1
 cd $TmpDir
 ln -sf docs/README.en.txt README.txt
 ln -sf bin/install install
-cd $SvnProject
 
 # 5 先壓縮精簡包
 
-tar -czvf $ShellTar $TmpDir --exclude=.svn
+tar -czvf $ShellTar $GoogleCodeCrawlzilla/$TmpDir --exclude=.svn
 checkMethod 5.1
 
 # 6 製作full package 檔
@@ -109,16 +103,16 @@ checkMethod 6.1
 #checkMethod 6.5
 
 # 7 將製作好的安裝檔放在保存的資料夾
-if [ -f $SvnProject/dist/$ShellTar ];then
-  rm $SvnProject/dist/$ShellTar;
+if [ -f $GoogleCodeCrawlzilla/dist/$ShellTar ];then
+  rm $GoogleCodeCrawlzilla/dist/$ShellTar;
   checkMethod 7.0
 fi
-if [ -f $SvnProject/dist/$StableTar ];then
-  rm $SvnProject/dist/$StableTar;
+if [ -f $GoogleCodeCrawlzilla/dist/$StableTar ];then
+  rm $GoogleCodeCrawlzilla/dist/$StableTar;
   checkMethod 7.0
 fi
-cp $ShellTar $SvnProject/dist/$StableTar
-mv $ShellTar $SvnProject/dist
+cp $ShellTar $GoogleCodeCrawlzilla/dist/$StableTar
+mv $ShellTar $GoogleCodeCrawlzilla/dist
 checkMethod 7.1
 # 7.2 刪除 DELETE_LOCK=1
 if [ $DELETE_LOCK -eq 1 ];then
@@ -137,11 +131,11 @@ if [ "$upload_sf" == "y" ];then
         USER=goldjay1231;
     fi
     # 上傳到sf.net
-    scp $SvnProject/dist/$ShellTar $USER,crawlzilla@frs.sourceforge.net:/home/frs/project/c/cr/crawlzilla/testing/Crawlzilla-$CURRENT_VER/
+    scp $GoogleCodeCrawlzilla/dist/$ShellTar $USER,crawlzilla@frs.sourceforge.net:/home/frs/project/c/cr/crawlzilla/testing/Crawlzilla-$CURRENT_VER/
     echo "最新的版本[ $ShellTar ]也同時上傳到sf.net囉"
     echo "http://sourceforge.net/downloads/crawlzilla/testing/Crawlzilla-$CURRENT_VER/"
 fi
 
 echo "完成，一切確認後，最後的檔案放在這個目錄內："
-echo "	$SvnProject/dist/$StableTar "
-
+echo "	$GoogleCodeCrawlzilla/dist/$StableTar "
+# 
