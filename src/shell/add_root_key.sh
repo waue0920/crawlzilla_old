2c5b745ded5a828cc662bdfa4b63753276dfee06
goldjay1231 <goldjay1231@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2010-12-13 09:22:50 +0000

    Add: add root key for automated installation (automated scp slave_install)

---
+++ src/shell/add_root_key.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# [Check root]
+if [ $USER != "root" ]; then
+    echo "Please change root to execute it !"
+    exit
+fi
+
+# [Variables Setup]
+# ex. slave_root_password='crawlzilla'
+# ex. slave_ips='192.168.0.1  192.168.0.2  192.168.0.3'
+slave_root_password=''
+slave_ips=''
+
+
+# [Main]
+# generate root ssh key
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""
+if [ -f "~/.ssh/authorized_keys" ]; then
+    cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+else
+    cp ~/.ssh/id_rsa.pub ~/.ssh/authorized_keys
+fi
+
+# copy to slaves
+for ip in $slave_ips
+do
+    expect -c "spawn scp -r /root/.ssh root@${ip}:~
+    set timeout 1
+    expect \"*:\"
+    send "$slave_root_password\r"
+    expect eof"
+done
