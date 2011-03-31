8703a5223029a095a433c223efbf198eee2e5e75
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2011-03-31 06:41:25 +0000

    # go.sh fixed

--- trunk/v1.0/opt/main/go.sh
+++ trunk/v1.0/opt/main/go.sh
@@ -21,135 +21,220 @@
 # Version:
 #    1.0
 # History:
-#   2010/06/07  Rock    First release (1.0)
+#   2011/03/16  waue	release (1.0)
+#   2010/06/07  Rock    First release (0.3)
 
-if [ "$1" == "" ]; then
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ]; then
  echo "1. 使用這個shell ，首先你需要有crawler這個使用者，並且hadoop 已經開始運作";
- echo "2. /home/crawler/crawlzilla/user/admin/meta/urls/urls.txt 裡面有你要抓的網址";
- echo "3. 執行 ./go.sh [深度] [資料夾名稱] 即可，如：";
- echo "	./go.sh 3 crawlname"
+ echo "2. 編輯 /home/crawler/crawlzilla/user/admin/meta/urls/url.txt ";
+ echo "3. 執行 ./go.sh <使用者名稱> <資料夾名稱> <深度> [<redo>]"
+ echo "ex1: ./go.sh admin crawlname 3"
+ echo "ex2: ./go.sh admin crawlname 3 redo"
  echo "4. 等nutch所有程序完成，則你的資料在 /home/crawler/crawlzilla/user/admin/IDB/crawlname/ "
  exit
 fi
 
-#META_PATH="/home/crawler/crawlzilla/.metadata"
-#ARCHIEVE_DIR="	/home/crawler/crawlzilla/user/admin/IDB/"
-#HADOOP_BIN="/opt/crawlzilla/nutch/bin"
-
-Depth=$1
+USERNAME=$1
 JNAME=$2
+Depth=$3
+if [ "$4" != "" ];then REDO=true;fi
 
-[ $USER != "crawler" ] && echo "please enter crawler's password"  && exec su crawler -c "$0 $Depth $JNAME"
+# env
+OptWebapp="/opt/crawlzilla/tomcat/webapps"
+OptNutchBin="/opt/crawlzilla/nutch/bin"
+HomeUserDir="/home/crawler/crawlzilla/user"
+HdfsHome="/user/crawler"
 
-source "/opt/crawlzilla/nutch/conf/hadoop-env.sh";
+# local para
+HomeUserTmp="$HomeUserDir/$USERNAME/tmp"
+HomeUserIDB="$HomeUserDir/$USERNAME/IDB"
+HomeUserWeb="$HomeUserDir/$USERNAME/web"
+HomeUserMeta="$HomeUserDir/$USERNAME/meta"
+
+
+
+
+[ $USER != "crawler" ] && echo "please enter crawler's password"  && exec su crawler -c "$0 $USERNAME $JNAME $Depth $REDO"
+
+# logging to default when starting
 source "/opt/crawlzilla/main/log.sh" crawl_go;
+source "/opt/crawlzilla/nutch/conf/hadoop-env.sh";
 
-function check_info ( )
+
+function check_tmp_info ( )
 {
   if [ $? -eq 0 ];then
-    show_info "[ok] $1";
+    show_tmp_info "[ok] $1";
   else
-    echo "error: $1 broken" > "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/status"
-    show_info "[error] $1 broken"
-    kill -9 $count_pid
+    echo "error: $1 broken" > "$HomeUserTmp/$JNAME/meta/status"
+    show_tmp_info "[error] $1 broken"
+    show_info $(cat $HomeUserTmp/$JNAME/meta/crawl.log)
+    #cat $HomeUserTmp/$JNAME/meta/crawl.log >> $LOG_SH_TARGET 2>&1
+    exit 8
+  fi
+}
+    
+function show_tmp_info ( ) 
+{
+    echo -e "\033[1;32;40m $1 \033[0m"
+    echo "< $(date +%H:%M:%S) > $1" >> $HomeUserTmp/$JNAME/meta/crawl.log 2>&1 
+}
+
+
+function check_idb_info ( )
+{
+  if [ $? -eq 0 ];then
+    show_idb_info "[ok] $1";
+  else
+    echo "error: $1 broken" > "$HomeUserIDB/$JNAME/meta/status"
+    show_idb_info "[error] $1 broken"
+    show_info $(cat $HomeUserTmp/$JNAME/meta/crawl.log)
+    #cat $HomeUserIDB/$JNAME/meta/crawl.log >> $LOG_SH_TARGET 2>&1
     exit 8
   fi
 }
 
-# [ begin ] 
+function show_idb_info ( ) 
+{
+    echo -e "\033[1;32;40m $1 \033[0m"
+    echo "< $(date +%H:%M:%S) > $1" >> $HomeUserIDB/$JNAME/meta/crawl.log 2>&1 
+}
+
 cd /home/crawler/crawlzilla/
 
-# check the same name before
-if [ -d /home/crawler/crawlzilla/user/admin/tmp/$JNAME ];then
-   mv /home/crawler/crawlzilla/user/admin/tmp/$JNAME /home/crawler/crawlzilla/user/admin/tmp/$JNAME-bek
+# check the same name in action !
+if [ -d $HomeUserTmp/$JNAME ];then
+   show_info "[error] $USER/$JNAME is existed! please check."; 
+   exit 0 ## exit if the same name in tmp/
 fi
-
-if [ ! -e "/home/crawler/crawlzilla/user/admin/tmp/$JNAME" ];then
-   mkdir -p "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta"
-   check_info "mkdir crawlStatusDir"
-fi
-
-
-# 存儲crawling狀態及花費時間的資料夾
-if [ ! -e "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta" ];then
-   mkdir -p "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta"
-   check_info "mkdir crawlStatusDir"
-fi
-
-echo $$ > "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/go.pid"
-check_info "$$ > /home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/go.pid"
-
-# 紀錄爬取深度
-echo $Depth > "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/depth"
-
-# 開始紀錄程序狀態
-echo "begin" > "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/status"
-echo "0" > "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/passtime"
-
-# 呼叫counter.sh紀錄時間
-/opt/crawlzilla/main/counter.sh $JNAME &
-sleep 5
-count_pid=$(cat "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/count.pid")
 
 # check the replicate directory on HDFS ; $? : 0 = shoud be delete
-/opt/crawlzilla/nutch/bin/hadoop dfs -test -e /user/crawler/admin/urls
+$OptNutchBin/hadoop dfs -test -e $HdfsHome/$USERNAME/${JNAME}_urls
 if [ $? -eq 0 ]; then
-  /opt/crawlzilla/nutch/bin/hadoop dfs -rmr /user/crawler/admin/urls
-  check_info "rmr urls"
+  $OptNutchBin/hadoop dfs -rmr $HdfsHome/$USERNAME/${JNAME}_urls
+  check_info "clean hdfs:${USERNAME}/${JNAME}_urls"
 fi
-/opt/crawlzilla/nutch/bin/hadoop dfs -test -e /user/crawler/admin/$JNAME
+$OptNutchBin/hadoop dfs -test -e $HdfsHome/$USERNAME/$JNAME
 if [ $? -eq 0 ]; then
-  /opt/crawlzilla/nutch/bin/hadoop dfs -mv /user/crawler/admin/$JNAME /user/crawler/admin/$JNAME-bek
-  check_info "mv $JNAME to $JNAME-bek"
+  $OptNutchBin/hadoop dfs -rmr $HdfsHome/$USERNAME/$JNAME 
+  check_info "clean hdfs:${USERNAME}/${JNAME} "
 fi
 
 
-/opt/crawlzilla/nutch/bin/hadoop dfs -mkdir /user/crawler/admin/$JNAME
-check_info "hadoop dfs -mkdir /user/crawler/admin/$JNAME"
+# check urls and redo if true then copy
+if [ "$REDO" == "ture" ] && [ -e $HomeUserIDB/$JNAME/meta/url.txt ]; then 
+  cp $HomeUserIDB/$JNAME/meta/url.txt $HomeUserMeta/urls/url.txt
+fi
+# check urls
+if [ ! -e $HomeUserMeta/urls/url.txt ];then 
+  show_info "[error] $HomeUserMeta/urls/url.txt is miss! please check.";
+  exit 0
+fi
 
-/opt/crawlzilla/nutch/bin/hadoop dfs -put /home/crawler/crawlzilla/user/admin/meta/urls /user/crawler/admin/urls
-check_info "hadoop dfs -put urls"
+# [ begin ] 
+
+BeginDate=$(date +%Y%m%d-%H:%M:%S)
+s_time=`date '+%s'`
+
+# make JNAME and its meta
+if [ ! -e "$HomeUserTmp/$JNAME/meta" ];then
+   mkdir -p "$HomeUserTmp/$JNAME/meta"
+   check_tmp_info "mkdir crawlStatusDir"
+fi
+
+# process id
+echo $$ > "$HomeUserTmp/$JNAME/meta/go.pid"
+check_tmp_info "$$ > $HomeUserTmp/$JNAME/meta/go.pid"
+
+# depth
+echo $Depth > "$HomeUserTmp/$JNAME/meta/depth"
+
+# status
+echo "begin" > "$HomeUserTmp/$JNAME/meta/status"
+
+# start time
+echo "$s_time" > "$HomeUserTmp/$JNAME/meta/starttime"
+echo "$BeginDate" > "$HomeUserTmp/$JNAME/meta/begindate"
+
+
+# make HDFS dir
+
+$OptNutchBin/hadoop dfs -mkdir $HdfsHome/$USERNAME/$JNAME
+check_tmp_info "hadoop dfs -mkdir $HdfsHome/$USERNAME/$JNAME"
+
+$OptNutchBin/hadoop dfs -put $HomeUserMeta/urls $HdfsHome/$USERNAME/${JNAME}_urls
+check_tmp_info "hadoop dfs -put urls"
 
 # nutch crawl begining
-echo "crawling" > "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/status"
+echo "crawling" > "$HomeUserTmp/$JNAME/meta/status"
 
-/opt/crawlzilla/nutch/bin/nutch crawl /user/crawler/admin/urls -dir /user/crawler/admin/$JNAME -depth $Depth -topN 5000 -threads 1000
-check_info "nutch crawl"
+show_tmp_info "nutch crawl begining..."
+$OptNutchBin/nutch crawl $HdfsHome/$USERNAME/${JNAME}_urls -dir $HdfsHome/$USERNAME/$JNAME -depth $Depth -topN 50000 -threads 1000
+check_tmp_info "nutch crawl finished"
 
-/opt/crawlzilla/nutch/bin/hadoop dfs -get /user/crawler/admin/$JNAME/* /home/crawler/crawlzilla/user/admin/tmp/$JNAME
-check_info "download search"
+# download Index DB from hdfs 
+$OptNutchBin/hadoop dfs -get $HdfsHome/$USERNAME/$JNAME/* $HomeUserTmp/$JNAME
+check_tmp_info "download Index DB"
 
+$OptNutchBin/hadoop dfs -get $HdfsHome/$USERNAME/${JNAME}_urls/url.txt $HomeUserTmp/$JNAME/meta/url.txt
+check_tmp_info "download url"
+
+# check IDB then mv to old-dir no matter REDO is true
+if [ -d $HomeUserIDB/$JNAME ]; then
+   mv $HomeUserIDB/$JNAME $HomeUserDir/$USERNAME/old/${JNAME}_${BeginDate} ## move the same name at IDB
+   check_tmp_info "move old to $HomeUserDir/$USERNAME/old/${JNAME}_${BeginDate}"
+fi
 
 # move the index-db from tmp to IDB
-mv /home/crawler/crawlzilla/user/admin/tmp/$JNAME /home/crawler/crawlzilla/user/admin/IDB/$JNAME
-check_info "mv indexdb from tmp to IDB"
+mv $HomeUserTmp/$JNAME $HomeUserIDB/$JNAME
+check_idb_info "mv indexDB from tmp to IDB"
 
-# check /home/crawler/crawlzilla/user/admin/web
-if [ ! -d /home/crawler/crawlzilla/user/admin/web ];then mkdir -p /home/crawler/crawlzilla/user/admin/web ;fi
-# check /opt/crawlzilla/tomcat/webapps/admin/
-if [ ! -d /opt/crawlzilla/tomcat/webapps/admin/ ];then mkdir -p /opt/crawlzilla/tomcat/webapps/admin/ ;fi
 
-# 製作 $JNAME 於 tomcat
-#cp -rf /opt/crawlzilla/tomcat/webapps/default /opt/crawlzilla/tomcat/webapps/$JNAME
-cp -rf /opt/crawlzilla/tomcat/webapps/default /home/crawler/crawlzilla/user/admin/web/$JNAME
-check_info "cp default to user/web/$JNAME"
-# link user/admin/web/JNAME to tomcat/webapps/admin/JNAME
-ln -sf /home/crawler/crawlzilla/user/admin/web/$JNAME /opt/crawlzilla/tomcat/webapps/admin/$JNAME
-check_info "link to tomcat/webapps"
+# make web index if without web link
+if [ ! -d $HomeUserWeb/$JNAME ];then
+   # prepare $JNAME at tomcat
+   cp -rf $OptWebapp/default $HomeUserWeb/$JNAME
+   check_idb_info "cp default to user/web/$JNAME"
+   # inject the nutch-site.xml for linking web and idb
+   sed -i '4s/XS_DIRX/'$USERNAME'\/IDB\/'$JNAME'/g' $HomeUserWeb/$JNAME/WEB-INF/classes/nutch-site.xml
+   check_idb_info "inject the nutch-site.xml for linking web and idb"
+fi
 
-# inject the nutch-site.xml for linking web and idb
-sed -i '8s/search/admin\/IDB\/'$JNAME'/g' /home/crawler/crawlzilla/user/admin/web/$JNAME/WEB-INF/classes/nutch-site.xml
-check_info "inject the nutch-site.xml for linking web and idb"
+# clean link for tomcat reload
+if [ -e $OptWebapp/${USERNAME}_$JNAME ] || [ "$REDO" == "true" ]; then
+   rm $OptWebapp/${USERNAME}_$JNAME
+fi
+# link user/$USERNAME/web/JNAME to tomcat/webapps/$USERNAME/JNAME
+ln -sf $HomeUserWeb/$JNAME $OptWebapp/${USERNAME}_$JNAME
+check_idb_info "link to tomcat/webapps"
 
-# 完成搜尋狀態 bedone
-#cp /home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/depth /home/crawler/crawlzilla/user/admin/IDB/$JNAME/
-#cp /home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/passtime /home/crawler/crawlzilla/user/admin/IDB/$JNAME/
+# clean meta-files
+rm "$HomeUserIDB/$JNAME/meta/go.pid"
 
-#if [ -d /home/crawler/crawlzilla/user/admin/IDB/$JNAME/ ];then
-#  cp -rf /home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta /home/crawler/crawlzilla/user/admin/tmp/$JNAME/metadata
-#fi
+$OptNutchBin/hadoop dfs -test -e $HdfsHome/$USERNAME/$JNAME
+if [ $? -eq 0 ]; then
+  $OptNutchBin/hadoop dfs -rmr $HdfsHome/$USERNAME/$JNAME
+  $OptNutchBin/hadoop dfs -rmr $HdfsHome/$USERNAME/${JNAME}_urls
+  check_idb_info "rmr $JNAME and ${JNAME}_bek on hdfs"
+fi
 
-echo "finish" > "/home/crawler/crawlzilla/user/admin/tmp/$JNAME/meta/status"
+# report status
+echo "finish" > "$HomeUserIDB/$JNAME/meta/status"
 
-kill -9 $count_pid
 
+
+# count pass time
+f_time=`date '+%s'`
+if [ "$s_time" == "" ] && [ -e $HomeUserTmp/$JNAME/meta/starttime ] ;then 
+  s_time=$(cat $HomeUserTmp/$JNAME/meta/starttime )
+fi
+pass_time=$(expr $f_time - $s_time)
+hours=$(expr $pass_time / 3600)
+pass_time=$(expr $pass_time % 3600)
+minutes=$(expr $pass_time / 60)
+seconds=$(expr $pass_time % 60)
+pass_time="$hours:$minutes:$seconds"
+show_idb_info "it takes : $pass_time"
+echo "$pass_time" > "$HomeUserIDB/$JNAME/meta/passtime"
+
+cat $HomeUserIDB/$JNAME/meta/crawl.log >> $LOG_SH_TARGET 2>&1
