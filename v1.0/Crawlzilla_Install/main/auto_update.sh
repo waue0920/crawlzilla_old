143a5219555833d8d8603ce5867641a02b902234
waue0920 <waue0920@de527d58-de6a-d65c-ef1f-909cbc6f6f8e>
2011-05-11 10:00:34 +0000

    # echo status info and fix bugs
    M    v1.0/Crawlzilla_Install/main/auto_update.sh

--- v1.0/Crawlzilla_Install/main/auto_update.sh
+++ v1.0/Crawlzilla_Install/main/auto_update.sh
@@ -4,34 +4,52 @@
 
 Update_Dir="/opt/crawlzilla/update"
 VER_CURR=$(cat /opt/crawlzilla/version)
+echo "This Crawlzilla is version: $VER_CURR"
 
 if [ -d $Update_Dir ];then
+  # "Clean Previous Update Files"
   rm -rf $Update_Dir/*
 else
   mkdir -p $Update_Dir
 fi
 
-wget http://sourceforge.net/projects/crawlzilla/files/testing/version/download -O $Update_Dir/version
+wget http://sourceforge.net/projects/crawlzilla/files/testing/version/download -O $Update_Dir/version > /dev/null 2>&1
+if [ ! -f $Update_Dir/version ];then
+  echo "[Error] Check Online Version Failed!"
+  exit 8
+fi
+
 VER_NEW=$(cat $Update_Dir/version)
+echo "The latest online version : $VER_NEW"
 
 if [ "$VER_NEW" == "$VER_CURR" ];then
   echo "Crawlzilla $VER_CURR is latest."
   exit 0
 fi
-read
-wget http://sourceforge.net/projects/crawlzilla/files/testing/Crawlzilla-1.0/Crawlzilla-$VER_NEW.tar.gz/download -O $Update_Dir/crawlzilla-update.tar.gz
 
-tar -xzvf $Update_Dir/crawlzilla-update.tar.gz -C $Update_Dir/
+wget http://sourceforge.net/projects/crawlzilla/files/testing/Crawlzilla-1.0/Crawlzilla-$VER_NEW.tar.gz/download -O $Update_Dir/crawlzilla-update.tar.gz > /dev/null 2>&1
 
-Work_Path="$Update_Dir/Crawlzilla_Install"
-cp -rf $Work_Path/main/*  /opt/crawlzilla/main/
-cp $Work_Path/version  /opt/crawlzilla/
+if [ ! -f $Update_Dir/crawlzilla-update.tar.gz ];then
+  echo "[Error] Download Crawlzilla-$VER_NEW.tar.gz Failed!"
+  exit 8
+else
+  echo "Download Crawlzilla-$VER_NEW.tar.gz Finish!" 
+fi
+
+tar -xzvf $Update_Dir/crawlzilla-update.tar.gz -C $Update_Dir/  > /dev/null 2>&1
+
+Work_Dir="$Update_Dir/Crawlzilla_Install"
+cp -rf $Work_Dir/main/*  /opt/crawlzilla/main/
+if [ $? == "0" ];then echo " Update main code"; fi
+
+cp $Work_Dir/version  /opt/crawlzilla/
 # copy crawlzilla.war to /opt/crawlzilla/tomcat/webapps
 mv /opt/crawlzilla/tomcat/webapps/crawlzilla.war $Update_Dir/
 mv /opt/crawlzilla/tomcat/webapps/crawlzilla $Update_Dir/crawlzilla-web
-cp $Work_Path/web/crawlzilla.war /opt/crawlzilla/tomcat/webapps/crawlzilla.war
+cp $Work_Dir/web/crawlzilla.war /opt/crawlzilla/tomcat/webapps/crawlzilla.war
+if [ $? == "0" ];then echo " Update Crawlzill websites"; fi
 
 #chown -R crawler /opt/crawlzilla/main/
 #chown -R crawler /opt/crawlzilla/tomcat/webapps/
 
-echo "System Update Finished"
+echo "Crawlizlla have been updated to version : $VER_NEW"
